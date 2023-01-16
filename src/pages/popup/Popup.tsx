e28e61d36867a35a18f01c295ffdcc62a2fed8a5
Nikolay Elesin <elesinn@ya.ru>
2023-01-16 17:43:44 -0600

    add

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from "react";
+import { useChromeStorageLocal } from "use-chrome-storage";
 
-export default function Popup(): JSX.Element {
+const Popup = () => {
+  const [url, setUrl] = React.useState<string | undefined>(undefined);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const [value, setValue, isPersistent, error] = useChromeStorageLocal(
+    "settings",
+    []
+  );
+  async function getCurrentTabUrl() {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    return tab?.url;
+  }
+
+  async function updateUrl(newOrigin: string) {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    const newUrlOr = new URL(newOrigin || "");
+    const url = new URL(tab?.url || "");
+    const newUrl = newUrlOr.origin + url.pathname + url.search;
+    chrome.tabs.update(tab.id || 0, { url: newUrl });
+  }
+
+  React.useEffect(() => {
+    getCurrentTabUrl().then((res) =>
+      setUrl(() => {
+        const url = res ? new URL(res) : undefined;
+        return url?.origin;
+      })
+    );
+  }, []);
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+    <div className="App flex flex-col justify-between h-screen p-2">
+      {/* Display as vertical list of url as buttons */}
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-bold p-2">Env switcher </h2>
+          <button
+            className="btn btn-error btn-xs"
+            onClick={() => {
+              setValue([]);
+            }}
+          >
+            clear
+          </button>
+        </div>
+
+        {value?.length
+          ? value.map((url: any, index: number) => (
+              <button
+                key={index}
+                className="btn btn-primary btn-xs"
+                onClick={() => updateUrl(url)}
+              >
+                {url}
+              </button>
+            ))
+          : `Add new envs, then click on them to switch`}
+      </div>
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label
+            htmlFor="my-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+
+          <p className="py-1">
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Env hostname</span>
+              </label>
+              <input
+                ref={inputRef}
+                type="text"
+                placeholder="https://my.best.app/"
+                className="input input-bordered w-full max-w-xs inpu"
+              />
+            </div>
+          </p>
+          <div className="modal-action">
+            <label
+              htmlFor="my-modal"
+              className="btn btn-sm"
+              onClick={() => {
+                if (inputRef.current) {
+                  setValue((prev: string[]) => [
+                    ...prev,
+                    inputRef.current?.value,
+                  ]);
+                }
+              }}
+            >
+              Add
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="my-modal" className="btn w-full btn-success btn-xs">
+          add new env
+        </label>
+      </div>
     </div>
   );
-}
+};
+
+export default Popup;
